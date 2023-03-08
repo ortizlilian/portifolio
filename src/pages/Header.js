370a444cab2d2a7709c841515321117c39fde786
Lily Dell <lily.starleks@gmail.com>
2023-03-08 19:14:10 +0000

    changing header navlink

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return <>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand"><NavLink to="/" end>Navbar</NavLink></a>
+            <NavLink to="/" end><a class="navbar-brand">Navbar</a></NavLink>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
